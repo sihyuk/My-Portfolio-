a80baffa283674213311958d654b312643a1a035
sihyuk <sihyukmhrjn@gmail.com>
2022-05-26 17:46:26 +0545

    First Commit

---
+++ pages/component/content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function content() {
+    return(
+        <div>
+
+        </div>)
+}
+
+export default content;
---
+++ pages/component/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from 
+
+function header() {
+    return(
+        <div>
+
+        </div>)
+}
+
+export default header;
